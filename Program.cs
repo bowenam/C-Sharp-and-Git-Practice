051d90dcd587fcb538b2b2b2c64559c7fe75a85e
bowenam <bowenam@etsu.edu>
2023-03-08 19:19:45 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Program
+{
+    class Program
+    {
+        public static string Elementary(double x, double y)
+        {
+            double addition = x + y;
+            double subtraction = x - y;
+            double multiplication = x * y;
+            double division;
+            if (y == 0)
+            {
+                return "You cannot divide by zero!\n\n";
+            }
+            else
+            {
+                division = x / y;
+            }
+            string message = $"Addition: {addition}\nSubtraction: {subtraction}\nMultiplication: {multiplication}\nDivision: {division}\n\n";
+            return message;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Elementary(0, 30));
+            Console.WriteLine(Elementary(2, 8));
+            Console.WriteLine(Elementary(12, 1.4));
+            Console.WriteLine(Elementary(7, 0));
+            Console.WriteLine(Elementary(9, 2));
+
+            //Car andrew = new("Andrew", "Nissan", "Altima", "maroon", 2010);
+            //Console.WriteLine($"My car is a {andrew.year} {andrew.make} {andrew.model}. Its color is {andrew.color}!");
+            //Console.WriteLine($"When {andrew.owner}'s {andrew.make} {andrew.model} honks, it says:");
+            //andrew.honk();
+        }
+    }
+}
---
+++ car.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+class Vehicle
+{
+    public string brand;
+    public void honk()
+    {
+        Console.WriteLine("Get outta the way!");
+    }
+}
+class Car : Vehicle
+{
+    public string owner, make, model, color;
+    public int year;
+    public Car(string carOwner, string carMake, string carModel, string carColor, int carYear)
+    {
+        owner = carOwner;
+        make = carMake;
+        model = carModel;
+        year = carYear;
+        color = carColor;
+    }
+}
